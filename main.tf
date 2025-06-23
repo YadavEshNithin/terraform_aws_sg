83f8d826f1bada23abdbf54be0b5983b89f814f7
YadavEshnithin <eshnithinyadav@gmail.com>
2025-06-23 17:30:31 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "frontend_allow_sg" {
   name        = var.frontend_allow_sg_name
-  description = var.sg_description
+  description = var.sg_desc
   vpc_id      = var.vpc_id
 
   tags = merge(
